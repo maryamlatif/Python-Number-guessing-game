45398274fecca588cd86f53f9e1227f29c43f874
maryamlatif <161033266+maryamlatif@users.noreply.github.com>
2025-02-18 07:25:28 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,18 @@
+"""import random
+guess_number = random.randint(1,20)
+
+print("I'm thinking of a number between 1 and 20")
+
+while True:
+    guess =int (input("Try your Luck:"))
+    
+    if guess < guess_number:
+        print("Your guess is too low")
+    elif  guess > guess_number:
+        print("Your guess is too high")
+    else:
+        print("Good job! You guessed right number")
+        break"""
+ 
+ 
+ 
